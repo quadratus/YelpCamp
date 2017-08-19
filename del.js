b197e693e1ef35b4e31ed436c104a0c64540c9a8
Amiteshwar Sidhu <amiteshwarsidhu7@gmail.com>
2017-08-19 10:22:23 +0530

    v3

--- del.js
+++ del.js
@@ -31,8 +31,8 @@ app.use(require("express-session")({
 app.use(passport.initialize());
 app.use(passport.session());
 passport.use(new LocalStrategy(User.authenticate()));
-passport.serializeUser(User.serializeUser);
-passport.deserializeUser(User.deserializeUser);
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 app.use(function(req, res, next){
     res.locals.currentUser = req.user;
